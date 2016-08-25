468d1fa029e6c9d18fbd91ad108d64630981ec8e
santosh10nov <santy10nov@gmail.com>
2016-08-25 07:19:00 +0530

    Favorite list

---
+++ ContactFav_ajax.php
@@ -0,0 +1,78 @@
+<?php
+    
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    
+    $pincode=$_GET['pin'];
+    $name=$_GET['name'];
+    $address=$_GET['address'];
+    $city=$_GET['city'];
+    $state=$_GET['state'];
+    $phone=$_GET['phone'];
+    $companyname=$_GET['companyname'];
+    $fav=$_GET['fav'];
+    $name_comp=$name."-".$companyname;
+    
+    $servername = "127.0.0.1";
+    $username = "root";
+    $pass = "yesbank";
+    $dbname = "transporter";
+    
+    
+    $arr=array($name_comp,$name,$companyname,$address,$city,$state,$pincode,$phone,$fav);
+    
+    //print_r($arr);
+    
+    try{
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        
+        $stmt1 = $conn->prepare("Select * from contact_list where phone='$arr[7]' and Pincode=$pincode");
+        $stmt1->execute();
+        
+        $numrows = $stmt1->rowCount();
+        
+        
+        if($fav==1){
+        
+        $sql = "INSERT INTO contact_list (NameCompany,Name,Company,Address,City,State,Pincode,phone,fav) VALUES ('$arr[0]','$arr[1]','$arr[2]','$arr[3]','$arr[4]','$arr[5]','$arr[6]','$arr[7]','$arr[8]')";
+        
+        // Add new contact-list
+        
+        $conn->exec($sql);
+       
+        echo $fav;
+            
+        }
+    
+        elseif($fav==0){
+            $row = $stmt1->fetch();
+            $userid=$row['userid'];
+            
+            // soft delete contact from contact-list
+            $sql = "UPDATE `contact_list` SET `fav`='$fav' WHERE `contact_list`.`userid` = $userid";
+            
+            $conn->exec($sql);
+            
+            echo $fav;
+        }
+        
+        elseif($numrows>0 and $fav==2){
+            $row = $stmt1->fetch();
+            $userid=$row['userid'];
+            
+            // Update the contact-list
+            
+            $sql = "UPDATE `contact_list` SET `NameCompany` = '$arr[0]', `Name` = '$arr[1]', `Company` = '$arr[2]', `Address` = '$arr[3]', `City` = '$arr[4]', `State` = '$arr[5]', `Pincode` = '$arr[6]', `phone` = '$arr[7]',`fav`=2 WHERE `contact_list`.`userid` = $userid";
+            
+            $conn->exec($sql);
+            
+            echo $fav;
+        }
+
+    }catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    
+    ?>
---
+++ SearchContact_ajax.php
@@ -0,0 +1,65 @@
+<?php
+    
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    
+    $searchname=$_GET['searchname'];
+    $output="";
+    
+    $servername = "127.0.0.1";
+    $username = "root";
+    $pass = "yesbank";
+    $dbname = "transporter";
+    
+    
+   
+    
+    //$arr=array($name_comp,$name,$companyname,$address,$city,$state,$pincode,$phone,$fav);
+    
+    //print_r($arr);
+    
+    try{
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        
+        //$stmt1 = $conn->prepare("Select * from contact_list where phone LIKE '$searchname' ");
+        $stmt1 = $conn->prepare("SELECT * FROM `contact_list` WHERE `Name` LIKE '%$searchname%' and fav!=0 Limit 5");
+
+        $stmt1->execute();
+        
+        $numrows = $stmt1->rowCount();
+        
+        
+        if($numrows==0){
+        
+        //$sql = "INSERT INTO contact_list (NameCompany,Name,Company,Address,City,State,Pincode,phone,fav) VALUES ('$arr[0]','$arr[1]','$arr[2]','$arr[3]','$arr[4]','$arr[5]','$arr[6]','$arr[7]','$arr[8]')";
+        
+        // Add new contact-list
+        
+        //$conn->exec($sql);
+       
+        echo "No Record Found";
+            
+        }
+    
+        elseif($numrows>0){
+            
+        
+            echo '<table class="table table-hover" id="try">';
+            echo '<thead><tr><th>Name</th><th>Company Name</th><th>Phone Number</th><th>Address</th></tr></thead><tboby>';
+            while($row = $stmt1->fetch()){
+                
+                
+                echo '<tr id="try" data-dismiss="modal" onclick="selectRow(\''.$row["Pincode"].'\',\''.$row["Name"].'\',\''.$row["Company"].'\',\''.$row["Address"].'\',\''.$row["City"].'\',\''.$row["State"].'\',\''.$row["phone"].'\')"><td>'.$row["Name"].'</td><td>'.$row["Company"].'</td><td>'.$row["phone"].'</td><td>'.$row["Address"].",".$row["City"].",".$row["State"]."-".$row["Pincode"].'</td></tr>';
+                
+                
+            }
+            echo '</tbody></table><br />';
+        }
+
+    }catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    
+    ?>
